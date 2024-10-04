29c161b27fa69684674420e955beef43a47090ec
GabrielT23 <gabaraujoteixeira@gmail.com>
2024-10-04 17:48:01 -0300

    feat:organizing-project-structure-and-adding-styled-components

---
+++ src/screens/Login/index.tsx
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { Container } from './styles';
+
+export default function Login() {
+  return (
+    <Container>
+      <Text>Teste</Text>
+      <StatusBar style="auto" />
+    </Container>
+  );
+}
